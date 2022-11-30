6efb22b187db4c11dff813aa20d85afb5f7e1a12
Daniel <daniel-mcintyre@hotmail.com>
2022-11-30 14:07:34 -0500

    additional style updates to header/nav component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
 import { FiShoppingCart } from 'react-icons/fi';
-import { RxPerson } from 'react-icons/rx';
+import { BsPerson } from 'react-icons/bs';
 
 function Header() {
   return (
     <>
     <nav>
       <div className="flex justify-between bg-slate-200 p-2">
-        <div className="nav-left-container">
+        <div className="nav-left-container ml-4">
             <div className="company logo">
               <Link to="/">E-Commm</Link>
             </div>
         </div>
-        <div className=" flex nav-right-container">
+        <div className=" flex nav-right-container mr-12">
              <div className="cart-container">
               <Link to="/cart">
                 <div className="flex cart-icon pr-5">
-                  <div className="pr-1">
+                  <div className="pr-1 mt-1">
                     <FiShoppingCart/>
                   </div>
                 <div className="cart-text">
@@ -28,8 +28,8 @@ function Header() {
             <div className="login-container">
             <Link to="/login">
               <div className="flex login-icon pr-5">
-                <div className="pr-1">
-                  <RxPerson/>
+                <div className="pr-1 mt-1">
+                  <BsPerson/>
                   </div>
                   <div className="login-text">
                   Login
